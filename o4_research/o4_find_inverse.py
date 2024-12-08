c684b2c8209b6df702ecd35a848e9d6c2900b34b
Ivan Fedorov <idfedorov09@gmail.com>
2024-12-08 22:14:36 +0300

    seems O(6) analogy failed

--- o4_research/o4_find_inverse.py
+++ o4_research/o4_find_inverse.py
@@ -1,11 +1,12 @@
 from math import exp
 
-from sympy import symbols, Matrix, diff, tanh, asin, log, cosh, I, sin, cos, latex, mathematica_code, simplify, atanh
+from sympy import symbols, Matrix, diff, tanh, asin, log, cosh, I, sin, cos, latex, mathematica_code, simplify, atanh, \
+    Function
 from o4_research.ccmodel import CCModel
 
 r, phi1, phi2 = symbols('r phi1 phi2')
 x, y, z = symbols('x y z')
-kappa, h = symbols('k h')
+kappa, h, a, b, c = symbols('k h a b c')
 
 g11 = 2 * kappa / h * 1 / (1 - r ** 2) * 1 / (1 - kappa ** 2 * r ** 2)
 g22 = 2 * kappa / h * (1 - r ** 2) / (1 - kappa ** 2 * r ** 2)
@@ -18,10 +19,14 @@ g_x = Matrix([
 
 model = CCModel(metric=g_x, coords_old=[r, phi1, phi2], coords_new=[x, y, z])
 
+FF = Function('F')(kappa)
+GG = Function('G')(kappa)
+
+
 @model.set_transformation(inverse = True)
 def transform(r, phi1, phi2):
-    x = log(r**2 / (1 - kappa**2 * r**2))
-    y = 2*log(tanh(phi2))
+    x = log(r**2 / (1 - kappa**2 * r**2)) + log(FF)
+    y = 2*log(tanh(phi2)) + log(GG)
     z = 2*phi1 - I * log ((1-kappa**2 * r**2) / (1-r**2))
     return x, y, z
 
